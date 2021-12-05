29e4bc7109704a68599de5213a9c31e77476c560
WeakCookie <tringuyen2762001@gmail.com>
2021-12-05 23:42:33 +0700

    fix conflict

--- final-project/src/main/java/com/Controller/registerServlet.java
+++ final-project/src/main/java/com/Controller/registerServlet.java
@@ -1,5 +1,4 @@
 package com.Controller;
-
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -9,43 +8,33 @@ import com.user.*;
 
 @WebServlet(urlPatterns = "/register")
 public class registerServlet extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String url = "/register.jsp";
+        String action = req.getParameter("action");
+        String message = "";
+        if(action == null)
+        {
+            action = "register";
+        }
+        if(action.equals("register"))
+        {
+            String email = req.getParameter("email");
+            String pass = req.getParameter("password");
+            String secondpass = req.getParameter("2password");
+            User user = new User();
+            if(pass.equals(secondpass))
+            {
+                user.setEmail(email);
+                user.setPass(pass);
+                user.setAdmin(0);
+                System.out.println(user.getEmail());
+                if(UserDAO.emailExists(user.getEmail()))
+                {
+                    url = "/register.jsp";
+                    message = "email have already exist";
 
-		String url = "/register.jsp";
-		String action = req.getParameter("action");
-		String message = "";
-		if (action == null) {
-			action = "register";
-		}
-		if (action.equals("register")) {
-			String email = req.getParameter("email");
-			String pass = req.getParameter("password");
-			String secondpass = req.getParameter("2password");
-			User user = new User();
-			if (pass.equals(secondpass)) {
-				user.setEmail(email);
-				user.setPass(pass);
-				user.setAdmin(0);
-				System.out.println(user.getEmail());
-				if (UserDAO.emailExists(user.getEmail())) {
-					url = "/register.jsp";
-					message = "email have already exist";
-
-				} else {
-					System.out.println(UserDAO.emailExists(user.getEmail()));
-					UserDAO.addUser(user);
-					message = "login";
-				}
-			} else {
-				message = "Password does not match!!!";
-				url = "/register.jsp";
-
-			}
-
-			req.setAttribute("message", message);
-
-<<<<<<< HEAD
                 }
                 else
                 {
@@ -61,17 +50,19 @@ public class registerServlet extends HttpServlet {
                 url = "/register.jsp";
                 
             }
-=======
-		} else if (action.equals("backtologin")) {
-			url = "/login.jsp";
-		}
-		getServletContext().getRequestDispatcher(url).forward(req, resp);
->>>>>>> 7977082450bb2b27a94e27e4b1b224000dc2f975
 
-	}
+            req.setAttribute("message", message);
+            
+        }
+        else if(action.equals("backtologin"))
+            {
+                url= "/login.jsp";
+            }
+        getServletContext().getRequestDispatcher(url).forward(req, resp);
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doPost(req, resp);
-	}
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
 }
